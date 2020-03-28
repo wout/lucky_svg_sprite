e639e36141c9e7aae7ae585d858f01643fd18dda
wout <wout@mick-wout.com>
2020-03-28 17:32:32 +0000

    Added more informative comments on template files.

--- templates/base_svg_icon.cr
+++ templates/base_svg_icon.cr
@@ -1,5 +1,5 @@
 # Use this intermediate class to customize how the icons are rendered to HTML.
-# More info:
+# More information available here:
 # https://github.com/tilishop/lucky_svg_sprite.cr#customizing-attributes
 abstract class BaseSvgIcon < LuckySvgSprite::Icon
   needs name : String
--- templates/base_svg_sprite.cr
+++ templates/base_svg_sprite.cr
@@ -1,5 +1,5 @@
 # Use this intermediate class to customize how the sprites are rendered to HTML.
-# More info:
+# More information available here:
 # https://github.com/tilishop/lucky_svg_sprite.cr#customizing-attributes
 abstract class BaseSvgSprite < LuckySvgSprite::Set
 end

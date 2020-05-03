b9329a762d179117794766552a419ec3a31cca55
wout <wout@mick-wout.com>
2020-05-03 08:23:38 +0100

    Add support for Crystal 0.34 and Lucky 0.21.

--- spec/lucky_minimum_setup.cr
+++
@@ -1,4 +0,0 @@
-# set up lucky loggor to avoid errors
-Lucky.configure do |settings|
-  settings.logger = Dexter::Logger.new(nil)
-end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,6 +1,5 @@
 require "spec"
 require "lucky"
-require "./lucky_minimum_setup"
 require "../src/lucky_svg_sprite"
 require "../src/lucky_svg_sprite/mixins/**"
 require "../src/lucky_svg_sprite/**"

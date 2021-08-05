d8b0d85e0b4166fe95254255ad5a6d2e54e6e995
Wout <hi@wout.codes>
2021-08-05 18:38:28 +0100

    test: fix version spec

--- spec/lucky_svg_sprite/version_spec.cr
+++ spec/lucky_svg_sprite/version_spec.cr
@@ -5,8 +5,8 @@ describe LuckySvgSprite::VERSION do
   describe "shard.yml" do
     it "matches the current version" do
       info = YAML.parse(File.read("./shard.yml"))
-      version = info["version"]
-      LuckySvgSprite::VERSION.should eq("v#{version}")
+
+      LuckySvgSprite::VERSION.should eq(info["version"])
     end
   end
 end

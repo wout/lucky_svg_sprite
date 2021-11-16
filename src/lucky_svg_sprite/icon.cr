d6881be1a72242dc1d5dfb825a295531a1cf101c
Wout <hi@wout.codes>
2021-11-16 19:58:44 +0100

    feat: add ability to render a raw icon

--- src/lucky_svg_sprite/icon.cr
+++ src/lucky_svg_sprite/icon.cr
@@ -6,4 +6,20 @@ abstract class LuckySvgSprite::Icon < Lucky::BaseComponent
       tag "use", "xlink:href": "#svg-#{set}-#{name}-icon"
     end
   end
+
+  class Raw < Lucky::BaseComponent
+    needs set : String
+    needs name : String
+    needs class_name : String? = nil
+
+    def render
+      tag "svg", class: class_name do
+        tag "use", "xlink:href": "#svg-#{@set}-#{@name}-icon"
+      end
+    end
+
+    private def class_name
+      @class_name || "svg-icon svg-#{@set}-icon svg-#{@set}-#{@name}-icon"
+    end
+  end
 end
